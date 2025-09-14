6197eb13d605df001152fb955287224ae4a7d4bf
sweetconsole <sweetconsole@gmail.com>
2025-09-15 02:07:12 +0300

    add block features

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,11 @@ const introVideo = document.getElementById("introVideo");
 let playingIntroVideo = false
 
 function toggleVideo() {
-	playingIntroVideo = !playingIntroVideo
-
 	if (playingIntroVideo) {
 		introVideo.load()
 	} else {
 		introVideo.play()
 	}
+
+	playingIntroVideo = !playingIntroVideo
 }
