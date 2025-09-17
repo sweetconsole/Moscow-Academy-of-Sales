d5605d1eb0d85641e017d0e541ddf1bfe8f7bedc
sweetconsole <sweetconsole@gmail.com>
2025-09-17 10:58:35 +0300

    Обновление

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,31 @@ function toggleVideo() {
 
 	playingIntroVideo = !playingIntroVideo
 }
+
+new Swiper(".partners-swiper", {
+	slidesPerView: 5,
+	spaceBetween: 57,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl:".partners-button-next",
+		prevEl: ".partners-button-prev",
+	},
+});
+
+new Swiper(".gallery_swiper", {
+	slidesPerView: 4,
+	spaceBetween: 30,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl:".gallery_button_next",
+		prevEl: ".gallery_button_prev",
+	},
+});
